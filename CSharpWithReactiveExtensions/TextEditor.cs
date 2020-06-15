cd1978ac8472612b8d4700e7908368e87aa60267
Juarez Júnior <juarez.a.s.junior@gmail.com>
2020-06-15 12:14:19 -0300

    Alterações.

---
+++ CSharpWithReactiveExtensions/TextEditor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace CSharpWithReactiveExtensions
+{
+    public class TextEditor
+    {
+        private readonly StringBuilder stringBuilder = new StringBuilder();
+
+        public event EventHandler<TextEditorLineArgs> BeforeWritingLine;
+
+        public event EventHandler<TextEditorLineArgs> AfterWritingLine;
+
+        public void WriteLine(string value)
+        {
+            BeforeWritingLine?.Invoke(this, new TextEditorLineArgs(value));
+
+            stringBuilder.Append(value);
+
+            AfterWritingLine?.Invoke(this, new TextEditorLineArgs(value));
+        }
+
+        public string GetText()
+        {
+            return stringBuilder.ToString();
+        }
+    }
+
+    public class TextEditorLineArgs
+    {
+        public string LineValue { get; internal set; }
+
+        public TextEditorLineArgs(string lineValue)
+        {
+            LineValue = lineValue;
+        }
+    }
+}
